dc87528950b7fb9cd1f89a64db1320ebfadef0a2
Krystian Potoczek <krystian.potoczek@zsz.bobowa.pl>
2024-10-17 10:21:34 +0200

    Szkola_ocena

---
+++ strona.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Dziennik</h1>
+    </header>
+    <main>
+        <h2>Matematyka</h2>
+        <table id="plan-lekcji">
+            <table border="1">
+        <thead>
+            <tr>
+                <th>Numer</th>
+                <th>Imie</th>
+                <th>Nazwisko</th>
+                <th>Ocena</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Jan</td>
+                <td>Kowalski</td>
+                <td>5</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Adam</td>
+                <td>Nowak</td>
+                <td>4</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Kamil</td>
+                <td>Tumidajski</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Artur</td>
+                <td>Kowal</td>
+                <td>2</td>
+        </tbody>
+    </table>
+    </main>
+    <aside>
+       <ul>
+        <li>Matematyka</li>
+        <li>J.Polski</li>
+        <li>J.Niemiecki</li>
+</ul>
+    </aside>
+
+    <footer>
+        ZSZ Bobowa
+    </footer>
+</body>
+</html>
